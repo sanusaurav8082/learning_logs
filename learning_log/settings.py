96d88367933c589ccb9cad22439149480eceb0ac
sanusaurav8082 <sanusaurav8082@gmail.com>
2019-01-21 13:01:12 +0530

    Seventh commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -141,7 +141,7 @@ if os.getcwd() == '/app':
 	BASE_DIR=os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT='staticfiles'
 	STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-	
+SITE_ID=2	
 	
 
 
